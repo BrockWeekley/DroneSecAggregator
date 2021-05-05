60af740293ec17a7e15c9b77fe622f194e17d80f
unknown <brockweekley@gmail.com>
2021-05-04 21:04:25 -0500

    Added parsing for powder log files

--- Converter.py
+++ Converter.py
@@ -1,6 +1,35 @@
 import pandas as pd
 import glob
 import math
+import re
+
+
+def parse_log(log_row, attribute):
+
+    log_information = [int(s) for s in re.findall(r'\b\d+\b', log_row)]
+    goodput = (log_information[2] * .008)
+    packets_received = log_information[1]
+    acks_received = log_information[0]
+    packets_lost = 0
+
+    if attribute == 'flow':
+        return flowbytes(goodput)
+    elif attribute == 'packet_length':
+        return packet_length(goodput, packets_received, acks_received, packets_lost)
+    elif attribute == 'subflow':
+        return subflow(goodput, packets_received, acks_received, packets_lost)
+    elif attribute == 'total_length':
+        return subflow(goodput, packets_received, acks_received, packets_lost)
+    elif attribute == 'init_win':
+        return init_win(goodput, packets_received, acks_received, packets_lost)
+    elif attribute == 'pkt_fwd':
+        return packets_received
+    elif attribute == 'iat_min':
+        return iatmin(packets_received)
+    elif attribute == 'bwd packets':
+        return acks_received
+    else:
+        return init_win(goodput, packets_received, acks_received, packets_lost)
 
 
 def flowbytes(goodput):
@@ -36,6 +65,8 @@ def packet_length(goodput, packets_received, acks_received, packets_lost):
 
 
 data = [pd.read_csv(filename) for filename in glob.glob('./SPIDER3D/*.csv')]
+powder_logs = [pd.read_csv(filename) for filename in glob.glob('./collection/*.log')]
+
 model_data = {
     'Flow Bytes /s': [],
     'BWD Packet Length Std': [],
@@ -80,7 +111,7 @@ for file in data:
         column['PacketsLost']
     ), axis=1)
     model_frame['Destination Port'] = 80
-    model_frame['Subflow Fwd Byutes'] = file_frame.apply(lambda column: subflow(
+    model_frame['Subflow Fwd Bytes'] = file_frame.apply(lambda column: subflow(
         column['Goodput(kbit/s)'],
         column['PacketsReceived'],
         column['ACKsReceived'],
@@ -109,6 +140,25 @@ for file in data:
         column['ACKsReceived'],
         column['PacketsLost']
     ), axis=1)
+
+    model_frame.to_csv('./ModelReadyData/model' + str(i) + '.csv', index=False, header=True)
+    i += 1
+
+for file in powder_logs:
+    file_frame = pd.DataFrame(file, columns=[
+        'Log'
+    ])
+
+    model_frame['Flow Bytes /s'] = file_frame.apply(lambda column: parse_log(column['Log'], 'flow'), axis=1)
+    model_frame['BWD Packet Length Std'] = file_frame.apply(lambda column: parse_log(column['Log'], 'packet_length'), axis=1)
+    model_frame['Destination Port'] = 80
+    model_frame['Subflow Fwd Bytes'] = file_frame.apply(lambda column: parse_log(column['Log'], 'subflow'), axis=1)
+    model_frame['Total Length of Fwd Packets'] = file_frame.apply(lambda column: parse_log(column['Log'], 'total_length'), axis=1)
+    model_frame['Init_Win_bytes_forward'] = file_frame.apply(lambda column: parse_log(column['Log'], 'init_win'), axis=1)
+    model_frame['act_data_pkt_fwd'] = file_frame.apply(lambda column: parse_log(column['Log'], 'pkt_fwd'), axis=1)
+    model_frame['Fwd IAT Min'] = file_frame.apply(lambda column: parse_log(column['Log'], 'iat_min'), axis=1)
+    model_frame['Bwd Packets /s'] = file_frame.apply(lambda column: parse_log(column['Log'], 'bwd_packets'), axis=1)
+    model_frame['Average Packet Size'] = file_frame.apply(lambda column: parse_log(column['Log'], 'packet_size'), axis=1)
 
     model_frame.to_csv('./ModelReadyData/model' + str(i) + '.csv', index=False, header=True)
     i += 1
